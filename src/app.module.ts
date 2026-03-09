84703dec9735354ed56f0b23a7302f890ce01b7e
Mario Salazar <mariosalazar.ms.10@gmail.com>
2026-03-09 05:57:11 -0500

    feat(trash): add new feature for trash modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app/controller/app.controller';
 import { AppService } from './app/service/app.service';
 import { HomeModule } from './app/module/home.module';
-import { ModulesUsingSQLServer2022Module } from './factory/sqlserver/modules-using-sqlserver2022.module';
+//import { ModulesUsingSQLServer2022Module } from './factory/sqlserver/modules-using-sqlserver2022.module';
 import { ModulesUsingSQLServer2000Module } from './factory/sqlserver/modules-using-sqlserver2000.module';
 @Module({
   imports: [
     HomeModule,
-    ModulesUsingSQLServer2022Module,
+    //ModulesUsingSQLServer2022Module,
     ModulesUsingSQLServer2000Module,
   ],
   controllers: [AppController],
